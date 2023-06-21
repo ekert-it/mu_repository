9c4d1f5d6cafae2b4627b0e4de030dc79d3c1871
Thomas Ekert <support@ekert.de>
2023-06-21 16:51:32 +0200

    build

--- app/src/main/java/de/ekert/mavendemo/app/BuildInfo.java
+++ app/src/main/java/de/ekert/mavendemo/app/BuildInfo.java
@@ -4,7 +4,7 @@
 package	de.ekert.mavendemo.app;
 
 public class BuildInfo {
-	private static final String VERSION = "2023-06-21-16-40-14";
+	private static final String VERSION = "2023-06-21-16-50-57";
 	private static final String VERSIONBUILDER = "thomase";
 
 	public static String getVersion(){
